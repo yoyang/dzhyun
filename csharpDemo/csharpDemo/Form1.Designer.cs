756541734e62c5d96f557795bf6113339210d064
杨 勇 <yangyong@gw.com.cn>
2015-06-17 17:54:42 +0800

    csharp

---
+++ csharpDemo/csharpDemo/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace csharpDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connect = new System.Windows.Forms.Button();
+            this.shutdown = new System.Windows.Forms.Button();
+            this.send = new System.Windows.Forms.Button();
+            this.txtRequest = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(28, 12);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(75, 23);
+            this.connect.TabIndex = 0;
+            this.connect.Text = "连接";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // shutdown
+            // 
+            this.shutdown.Location = new System.Drawing.Point(134, 12);
+            this.shutdown.Name = "shutdown";
+            this.shutdown.Size = new System.Drawing.Size(75, 23);
+            this.shutdown.TabIndex = 1;
+            this.shutdown.Text = "断开";
+            this.shutdown.UseVisualStyleBackColor = true;
+            this.shutdown.Click += new System.EventHandler(this.shutdown_Click);
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(732, 41);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(75, 23);
+            this.send.TabIndex = 2;
+            this.send.Text = "发送";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // txtRequest
+            // 
+            this.txtRequest.Location = new System.Drawing.Point(59, 41);
+            this.txtRequest.Name = "txtRequest";
+            this.txtRequest.Size = new System.Drawing.Size(667, 21);
+            this.txtRequest.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "请求";
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(26, 98);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResponse.Size = new System.Drawing.Size(862, 301);
+            this.txtResponse.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "结果";
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(813, 41);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 7;
+            this.cancel.Text = "取消";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 402);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRequest);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.shutdown);
+            this.Controls.Add(this.connect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Button shutdown;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.TextBox txtRequest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button cancel;
+    }
+}
+
---
+++ csharpDemo/csharpDemo/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WebSocket4Net;
+using SuperSocket.ClientEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace csharpDemo
+{
+   
+    public partial class Form1 : Form
+    {
+
+        String strMessage;
+        JObject objJson;
+        String strRequestId;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        WebSocket websocket;
+        private void websocket_Opened(object sender, EventArgs e)
+        {
+            strMessage = "连接成功";
+            this.txtResponse.Invoke(new EventHandler(ShowMessage), e);            
+        }
+        private void websocket_Closed(object sender, EventArgs e)
+        {
+            strMessage = "连接关闭";
+            this.txtResponse.Invoke(new EventHandler(ShowMessage), e); 
+        }
+        private void websocket_MessageReceived(object sender, DataReceivedEventArgs e)
+        {
+            strMessage = Encoding.UTF8.GetString(e.Data);
+            objJson = JObject.Parse(strMessage);
+            strRequestId = objJson.Value<string>("req_seq");
+            strMessage = objJson.ToString();
+            this.txtResponse.Invoke(new EventHandler(ShowMessage), e); 
+        }
+        private void websocket_Error(object sender, ErrorEventArgs e)
+        {
+            strMessage = e.Exception.ToString();
+            this.txtResponse.Invoke(new EventHandler(ShowMessage), e); 
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            websocket = new WebSocket("ws://114.80.136.56:20718/dataproxy");
+            websocket.Opened += new EventHandler(websocket_Opened);
+            websocket.Error += new EventHandler<ErrorEventArgs>(websocket_Error);
+            websocket.Closed += new EventHandler(websocket_Closed);
+            websocket.DataReceived += new EventHandler<DataReceivedEventArgs>(websocket_MessageReceived);
+            websocket.Open();
+        }
+
+        private void shutdown_Click(object sender, EventArgs e)
+        {
+            websocket.Close();
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+            this.websocket.Send(txtRequest.Text.Trim());
+        }
+        private void ShowMessage(object sender, EventArgs e)
+        {
+            this.txtResponse.Text = strMessage;
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            this.websocket.Send("/json/cancel?" + strRequestId);
+            this.txtResponse.Text = "请求取消";
+        }
+    }
+}
